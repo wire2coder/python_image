e9d80e3c56eb8edad75e22f5502652eb21d1ad7b
root <wirenite2@gmail.com>
2019-02-06 10:36:53 +0000

    compiled, finished TODO 6, looking at final result

--- print_results.py
+++ print_results.py
@@ -81,9 +81,31 @@ def print_results(results_dic, results_stats_dic, model,
     
     for key in results_stats_dic:
         # TODO: 6b HERE
-        if "p" in results_stats_dic[key]:
-            print(key)
-            HERE 2/5
+        if key.startswith('p'):
+            print("\n{:20}: {:.2f}".format(key, results_stats_dic[key] ) )
+    
+    # IF print_incorrect_dogs == True AND there were images incorrectly 
+    # classified as dogs or vice versa - print out these cases
+    if (print_incorrect_dogs and ( (results_stats_dic['n_correct_dogs'] + results_stats_dic['n_correct_notdogs']) != results_stats_dic['n_images'] ) ):
+        print("\nINCORRECT Dog/NOT Dog Assignments:")
+        
+        # process through results dict, printing incorrectly classified dogs
+        for key in results_dic:
+            # TODO 6c
+            if (results_dic[key][3] == 1 and results_dic[key][4] == 0):
+                print("pet label: {}, classifier label: {}".format("pet label", "classifier label"))
+            
+        # IF print_incorrect_breed == True AND there were dogs whose breeds 
+        # were incorrectly classified - print out these cases                    
+        if (print_incorrect_breed and (results_stats_dic['n_correct_dogs'] != results_stats_dic['n_correct_breed']) ):
+            print("\nINCORRECT Dog Breed Assignment:")
+            
+            # process through results dict, printing incorrectly classified breeds
+            for key in results_dic:
+                
+                # Pet Image Label is-a-Dog, classified as-a-dog but is WRONG breed
+                if ( sum(results_dic[key][3:]) == 2 and results_dic[key][2] == 0 ):
+                    print("Real: {:>26}   Classifier: {:>30}".format( results_dic[key][0], results_dic[key][1]) )
         
     
     # end of the function
